62f7cecb1b1a1b4ae85a38094100d6b737dc7306
aldebaran2309 <129069496+aldebaran2309@users.noreply.github.com>
2024-09-03 00:44:06 +0530

    Update ModelId.java

--- src/tradingengine/model/ModelId.java
+++ src/tradingengine/model/ModelId.java
@@ -1,5 +1,19 @@
 package tradingengine.model;
 
+public class ModelId extends Model {
+
+    private int id;
+
+    public int getId() {
+	return id;
+    }
+
+    public void setId(int id) {
+	this.id = id;
+    }
+}
+package tradingengine.model;
+
 public class ModelId {
 
 }
