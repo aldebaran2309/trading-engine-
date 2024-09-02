342fb950647c0a934600d3f335c1178b4cae4695
aldebaran2309 <129069496+aldebaran2309@users.noreply.github.com>
2024-09-03 00:47:22 +0530

    Update Seller.java

--- src/tradingengine/model/Seller.java
+++ src/tradingengine/model/Seller.java
@@ -1,5 +1,114 @@
 package tradingengine.model;
 
+import java.lang.runtime.ObjectMethods;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.xml.catalog.CatalogManager;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import tradingengine.model.TradingEngine.Listener;
+
 public class Seller {
+
+    private static final Logger LOGGER = CatalogManager.getLogger("seller");
+
+    private List<SalesOrder> salesOrders = new ArrayList<>();
+    private String name;
+
+    public Listener listener;
+
+    public Seller(String name) {
+	this.name = name;
+    }
+
+    public void addSalesOrder(SalesOrder salesOrder) {https://intellij-support.jetbrains.com/hc/en-us/articles/4407356204178-IDE-Can-t-Connect-to-JetBrains-Account
+	((Object) LOGGER).debug(name + " adding " + salesOrder);
+	salesOrder.setSeller(this);
+	this.salesOrders.add(salesOrder);
+    }
+
+    public boolean hasProduct(String productId) {
+	return this.salesOrders.stream().anyMatch(so -> {
+	    return so.getProductId().equals(productId);
+	});
+    }
+
+    /**
+     * @return {SalesOrder} the sales order for the given product that has the
+     *         lowest price
+     */
+    public SalesOrder getCheapestSalesOrder(String productId) {
+	return this.salesOrders.stream().filter(so -> {
+	    return so.getProductId().equals(productId);
+	}).sorted((o1, o2) -> Double.compare(o1.getPrice(), o2.getPrice()))
+		.findFirst().get();
+    }
+
+    public void removeSalesOrder(SalesOrder salesOrder) {
+	this.salesOrders = this.salesOrders.stream().filter(so -> {
+	    return !salesOrder.equals(so);
+	}).collect(Collectors.toList());
+    }
+
+    /** @return the out of date ones */
+    public List<SalesOrder> removeOutdatedSalesOrders(long ageInMs) {
+	long now = System.currentTimeMillis();
+
+	Map<Boolean, List<SalesOrder>> partitioned = salesOrders.stream()
+		.collect(Collectors.groupingBy(so -> {
+		    return now - so.getCreated().getTime() > ageInMs;
+		}));
+
+	this.salesOrders = ObjectMethods.defaultIfNull(
+		partitioned.get(Boolean.FALSE),
+		Collections.synchronizedList(new ArrayList<SalesOrder>()));
+	return ObjectUtils.defaultIfNull(partitioned.get(Boolean.TRUE),
+		Collections.synchronizedList(new ArrayList<SalesOrder>()));
+    }
+
+    public List<SalesOrder> getSalesOrders() {
+	return salesOrders;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((name == null) ? 0 : name.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	Seller other = (Seller) obj;
+	if (name == null) {
+	    if (other.name != null)
+		return false;
+	} else if (!name.equals(other.name))
+	    return false;
+	return true;
+    }
+
+    @Override
+    public String toString() {
+	return "Seller [name=" + name + "]";
+    }
 
 }
